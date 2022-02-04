4639acd113b17b7a5aabcc6bb564b965f4fe9798
Saurav Adhikari <sauravads123@gmail.com>
2022-02-04 09:40:08 +0545

    feat : automated env secrets creation

--- main.py
+++ main.py
@@ -26,10 +26,22 @@ data = json.load(env_file)
 # CREATE API
 api = GhApi(owner=GITHUB_REPO_OWNER, repo=GITHUB_REPO, token=GITHUB_ACCESS_TOKEN)
 
-logger.info(api.__dict__.values())
+# logger.info(api.__dict__.values())
+def encrypt(public_key: str, secret_value: str) -> str:
+    """Encrypt a Unicode string using the public key."""
+    public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
+    sealed_box = public.SealedBox(public_key)
+    encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
+    return b64encode(encrypted).decode("utf-8")
+
 
 if ENVIRONMENT != "REPO":
     env_exists = False
+    logger.info(f'Fetching repository information for {GITHUB_REPO}')
+    """
+    Repository id is required for public key generation
+    """
+    repoId = api.repos.get().id
     environments_response = api.repos.get_all_environments()
     if(environments_response.total_count > 0):
         for env in environments_response.environments:
@@ -42,18 +54,15 @@ if ENVIRONMENT != "REPO":
         api.repos.create_or_update_environment(ENVIRONMENT)
         logger.info(f'Created environment {ENVIRONMENT}')
     
-    logger.info(f'Populating environment secrets for {ENVIRONMENT}')
-    #api.actions.get_environment_public_key(repository_id, environment_name)
+    logger.info(f'Fetching environment public key for {ENVIRONMENT}')
+    public_key= api.actions.get_environment_public_key(repoId, ENVIRONMENT)
     #api.actions.create_or_update_environment_secret(repository_id, environment_name, secret_name, encrypted_value: str = None, key_id: str = None)
+    for key in data.keys(): 
+        encrypted_value=encrypt(public_key.key,data[key])
+        api.actions.create_or_update_environment_secret(repoId, ENVIRONMENT, key, encrypted_value, public_key.key_id)
 else:
     logger.info(f'Adding repository secrets')
     #api.actions.get_repo_public_key()
     #api.actions.create_or_update_repo_secret(secret_name, encrypted_value: str = None, key_id: str = None)
     
     
-def encrypt(public_key: str, secret_value: str) -> str:
-    """Encrypt a Unicode string using the public key."""
-    public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
-    sealed_box = public.SealedBox(public_key)
-    encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
-    return b64encode(encrypted).decode("utf-8")
