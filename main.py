f3b88bf542d47456ff497092c171e79a65d2c044
Saurav Adhikari <sauravads123@gmail.com>
2022-02-24 22:09:45 +0545

    feat : added support for base64 encoding

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ from ghapi.all import GhApi
 from loguru import logger
 from base64 import b64encode
 from nacl import encoding, public
-
+from argparse import ArgumentParser
+import base64
 # Load GIT HUB CREDENTIALS
 load_dotenv("./git.env")
 GITHUB_ACCESS_TOKEN = os.getenv('GITHUB_ACCESS_TOKEN')
 GITHUB_REPO_OWNER = os.getenv('GITHUB_REPO_OWNER')
 GITHUB_REPO = os.getenv('GITHUB_REPO')
 ENVIRONMENT = os.getenv('ENVIRONMENT')
+
+# Parse command line arguments
+parser = ArgumentParser()
+
+parser.add_argument("--base64", nargs='+',
+                    help="Base64 encode secrets", metavar="key")
+
+args = parser.parse_args()
+base64_keys = args.base64 if args.base64 is not None else []
 
 api = GhApi(owner=GITHUB_REPO_OWNER, repo=GITHUB_REPO, token=GITHUB_ACCESS_TOKEN)
 
@@ -29,6 +39,12 @@ def get_env_data_as_dict(path: str) -> dict:
        return dict(tuple(line.replace('\n', '').split('=')) for line
                 in f.readlines() if not line.startswith('#'))
 
+# Function to Base64 encode string
+def base64_encode(value:str)-> str:
+    value_bytes = value.encode('ascii')
+    base64_bytes = base64.b64encode(value_bytes)
+    return base64_bytes.decode('ascii')
+
 # LOAD ENV FILE
 env_file_switch={
                 "DEV":'./dev.env',
@@ -43,30 +59,33 @@ env_data = get_env_data_as_dict(env_file_path)
 env_keys_set = set(env_data.keys())
 
 if ENVIRONMENT != "REPO":
-    logger.info(f'Fetching repository information for {GITHUB_REPO}')
+    logger.info(f'Fetching repository information for :: {GITHUB_REPO}')
     repoId = api.repos.get().id
     #(repository_id, environment_name, per_page, page)
     github_env_secrets = api.actions.list_environment_secrets(repoId,ENVIRONMENT,1000,0)
     github_env_secrets_keys_set = set([x.name for x in github_env_secrets.secrets])
     #Getting asymmetric difference ignoring order
     secrets_diff = github_env_secrets_keys_set - env_keys_set
-    logger.info(f'Performing secrets cleanup for environment: {ENVIRONMENT}')
+    logger.info(f'Performing secrets cleanup for environment :: {ENVIRONMENT}')
     for key in secrets_diff:
-        logger.info(f'Removing secret `{key}` from {ENVIRONMENT} in GitHub')
+        logger.info(f'Removing secret `{key}` from `{ENVIRONMENT}` in GitHub')
         api.actions.delete_environment_secret(repoId, ENVIRONMENT, key)
-    logger.info(f'Creating/Updating new environment {ENVIRONMENT}')
+    logger.info(f'Creating/Updating new environment :: {ENVIRONMENT}')
     api.repos.create_or_update_environment(ENVIRONMENT)
-    logger.info(f'Created/Updated environment {ENVIRONMENT}')
+    logger.info(f'Created/Updated environment :: {ENVIRONMENT}')
 
-    logger.info(f'Fetching environment public key for {ENVIRONMENT}')
+    logger.info(f'Fetching environment public key for :: {ENVIRONMENT}')
     public_key= api.actions.get_environment_public_key(repoId, ENVIRONMENT)
     for key,value in env_data.items(): 
+        if key in base64_keys:
+            logger.info(f'Base64 encoding secret :: {key}')
+            value=base64_encode(value)
         encrypted_value=encrypt(public_key.key,value)
-        logger.info(f'Adding environment secret {key} for {ENVIRONMENT}')
+        logger.info(f'Adding environment secret `{key}` for :: {ENVIRONMENT}')
         api.actions.create_or_update_environment_secret(repoId, ENVIRONMENT, key, encrypted_value, public_key.key_id)
-        logger.info(f'Successfully added environment secret {key} for {ENVIRONMENT}')
+        logger.info(f'Successfully added environment secret `{key}` for {ENVIRONMENT}')
 else:
-    logger.info(f'Performing secrets cleanup for repository: {GITHUB_REPO}')
+    logger.info(f'Performing secrets cleanup for repository :: {GITHUB_REPO}')
     repo_secrets= api.actions.list_repo_secrets(1000,0)
     repo_secrets_keys_set = set([x.name for x in repo_secrets.secrets])
     #Getting asymmetric difference ignoring order
@@ -79,7 +98,10 @@ else:
     public_key=api.actions.get_repo_public_key()
 
     for key,value in env_data.items(): 
+        if key in base64_keys:
+            logger.info(f'Base64 encoding secret :: {key}')
+            value=base64_encode(value)
         encrypted_value=encrypt(public_key.key,value)
-        logger.info(f'Adding repository secret {key}')
+        logger.info(f'Adding repository secret :: {key}')
         api.actions.create_or_update_repo_secret(key, encrypted_value, public_key.key_id)    
-        logger.info(f'Successfully added repository secret {key}')
+        logger.info(f'Successfully added repository secret :: {key}')
